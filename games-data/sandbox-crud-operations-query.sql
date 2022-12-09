a05d3fbd823fcb8564a5152941e0ac2922cd9755
[robless28] <[maria.gonzalezrobles01@utrgv.edu]>
2022-12-08 20:17:43 -0600

    Add games data project

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+--INSERT INTO sandbox(int16_value, string_value, datetime_value) VALUES (2045, 'goodbye world', '1900-02-15 11:11:11');
+
+--UPDATE sandbox SET int32_value = -1024620 WHERE id = 77;
+
+--DELETE FROM sandbox WHERE id = 85;
+
+SELECT * FROM sandbox;
